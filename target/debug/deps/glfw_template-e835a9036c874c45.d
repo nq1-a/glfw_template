231cb7fba8e7d77df5437f238148cd5fd63748a1
styrenediene <nyquildrinker1@gmail.com>
2025-12-30 21:48:21 -0600

    testing rust

---
+++ target/debug/deps/glfw_template-e835a9036c874c45.d
@@ -0,0 +1,5 @@
+/home/xenontf/Projects/glfw_template/target/debug/deps/glfw_template-e835a9036c874c45.d: src/main.rs
+
+/home/xenontf/Projects/glfw_template/target/debug/deps/glfw_template-e835a9036c874c45: src/main.rs
+
+src/main.rs:
---
+++ target/debug/glfw_template.d
@@ -0,0 +1,1 @@
+/home/xenontf/Projects/glfw_template/target/debug/glfw_template: /home/xenontf/Projects/glfw_template/src/main.rs
